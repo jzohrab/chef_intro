e4c176b40bbe854702c51bd65b421997c9efb8ae
Jeff Zohrab <jzohrab@gmail.com>
2016-06-07 16:28:22 -0400

    Initial generated cookbook (boilerplate).

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'chef_intro'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef_intro'
+long_description 'Installs/Configures chef_intro'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef_intro
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
